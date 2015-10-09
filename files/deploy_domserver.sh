473edd79b84a365406cacc10c6005e31678c8167
Keith Johnson <kjohns07@cs.fit.edu>
2015-10-09 15:51:39 -0400

    Update files

--- files/deploy_domserver.sh
+++ files/deploy_domserver.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
-# Expects to find DOMSERVER_S3_FILE and DOMSERVER_S3_BUCKET in the env variables
+#!/bin/bash -ex
+# Expects to find DOMSERVER_S3_BUCKET, DOMSERVER_S3_REGION, DOMSERVER_S3_FILE in the env variables
 
 INSTALLDIR="/srv/${DOMSERVER_S3_FILE%%.*}"
 mkdir -p $INSTALLDIR
 
-aws s3 cp s3://${DOMSERVER_S3_BUCKET}/${DOMSERVER_S3_FILE} /tmp/${DOMSERVER_S3_FILE}
+aws s3 cp --region=${DOMSERVER_S3_REGION} s3://${DOMSERVER_S3_BUCKET}/${DOMSERVER_S3_FILE} /tmp/${DOMSERVER_S3_FILE}
 tar zxf /tmp/${DOMSERVER_S3_FILE} -C "$INSTALLDIR"
 
 # run script to install/post configuration
 cd $INSTALLDIR
+chmod +x $INSTALLDIR/setup.sh
 ./setup.sh
 
 # now we configure apache2, and reload it
