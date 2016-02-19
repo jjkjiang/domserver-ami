de36a5d05da2f3b7541c3bfa238f65c19d04d636
Keith Johnson <kjohns07@cs.fit.edu>
2016-02-19 10:40:48 -0500

    Support the special filename "latest"
    This will work in conjunction with the prepare.sh script in the
    domjudge-aws repository to get the latest tar.gz archive from S3. Saves
    running terraform every time you want to update DOMjudge.

--- files/deploy_domserver.sh
+++ files/deploy_domserver.sh
@@ -1,11 +1,18 @@
 #!/bin/bash -ex
 # Expects to find DOMSERVER_S3_BUCKET, DOMSERVER_S3_REGION, DOMSERVER_S3_FILE in the env variables
 
+# Special case the filename "latest" and assume it's a link to the latest file
+if [ ${DOMSERVER_S3_FILE} == "latest" ]; then
+  aws s3 cp --region=${DOMSERVER_S3_REGION} s3://${DOMSERVER_S3_BUCKET}/${DOMSERVER_S3_FILE} /tmp/domserver-latest-version
+  DOMSERVER_S3_FILE=$(cat /tmp/domserver-latest-version)
+fi
+
 INSTALLDIR="/srv/${DOMSERVER_S3_FILE%%.*}"
 mkdir -p $INSTALLDIR
 
-aws s3 cp --region=${DOMSERVER_S3_REGION} s3://${DOMSERVER_S3_BUCKET}/${DOMSERVER_S3_FILE} /tmp/${DOMSERVER_S3_FILE}
+aws s3 cp --region=${DOMSERVER_S3_REGION} s3://${DOMSERVER_S3_BUCKET}/${DOMSERVER_S3_FILE} "/tmp/${DOMSERVER_S3_FILE}"
 tar --no-same-owner -zxf /tmp/${DOMSERVER_S3_FILE} -C "$INSTALLDIR"
+rm "/tmp/${DOMSERVER_S3_FILE}"
 
 # run script to install/post configuration
 cd $INSTALLDIR
